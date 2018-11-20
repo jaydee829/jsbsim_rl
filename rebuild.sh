f16a3d883ef3adc09a0ed9254b322f96d187dde9
Josh Bertram <bertram1@iastate.edu>
2018-11-20 16:14:50 -0600

    Renamed docker image name

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,1 @@
-docker build -t jrjbertram/jsbsim . && ( docker rm jsbsim; docker run -p 6901:6901 -p 8008:8008 --name jsbsim -v model:/home/jsbsim/model jrjbertram/jsbsim  )
+docker build -t jrjbertram/jsbsim_rl . && ( docker rm jsbsim; docker run -p 6901:6901 -p 8008:8008 --name jsbsim -v model:/home/jsbsim/model jrjbertram/jsbsim_rl  )
